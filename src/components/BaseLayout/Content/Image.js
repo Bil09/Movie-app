76d037133059fca395dc341964a76c559505cbac
Bilhid <bil.bilhid09@gmail.com>
2022-01-27 18:18:27 +0100

    overlay added and visual bug with the dropdown fixed

---
+++ src/components/BaseLayout/Content/Image.js
@@ -0,0 +1,11 @@
+import styles from '../../../styles/Sass/Content.module.scss';
+const Image = (props) => {
+   return(
+      <div className={styles.content__row__image}>
+         <img src={props.image} alt="image" className={styles.content__row__image__img} />
+         <div className={styles.content__row__image__overlay}></div>
+      </div>
+   );
+};
+
+export default Image;
--- src/components/BaseLayout/Content/Row.js
+++ src/components/BaseLayout/Content/Row.js
@@ -1,16 +1,19 @@
 import styles from '../../../styles/Sass/Content.module.scss';
 import img from '../../../furious.jpg'
+import Image from './Image';
 const Row = (props) => {
    return(
       <div className={styles.content__row}>
-         <img src={img} alt="image" className={styles.content__row__img} />
-         <img src={img} alt="image" className={styles.content__row__img} />
-         <img src={img} alt="image" className={styles.content__row__img} />
-         <img src={img} alt="image" className={styles.content__row__img} />
-         <img src={img} alt="image" className={styles.content__row__img} />
-         <img src={img} alt="image" className={styles.content__row__img} />
-         <img src={img} alt="image" className={styles.content__row__img} />
-         <img src={img} alt="image" className={styles.content__row__img} />
+         <Image image={img} />
+         <Image image={img} />
+         <Image image={img} />
+         <Image image={img} />
+         <Image image={img} />
+         <Image image={img} />
+         <Image image={img} />
+         <Image image={img} />
+         <Image image={img} />
+         <Image image={img} />
       </div>
    );
 };
